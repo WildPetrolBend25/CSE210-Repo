a6a72619a835b233c33cbd63f0fca3262cafc643
BeniaminoPetrini <pet22013@byui.edu>
2025-10-30 14:45:46 -0600

    Get grade letter A

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,12 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Console.WriteLine("Hello Prep2 World!");
 
         //initialize all the required variables
         Console.Write("What is your letter grade? ");
         string userinput = Console.ReadLine();
         int gradePercentage = int.Parse(userinput);
+
+        //Print user input
         Console.WriteLine($"This is your grade percentage {gradePercentage}");
+
+        string letterGrade = "";
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("You got an A!");
+            letterGrade = "A";
+        }
+        Console.WriteLine(letterGrade);
     }
 }
