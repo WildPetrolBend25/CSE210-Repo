49be08a4d0936c279cc372f9c49c838c42209db3
BeniaminoPetrini <pet22013@byui.edu>
2025-10-30 14:09:27 -0600

    made chnages

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,12 +2,16 @@ using System;
 
 class Program
 {
+    // static void = can call the function without a return value.
+    //must always return a value except when calling void
     static void Main(string[] args)
     {
+        //write line moves the cursor to the next line
         Console.WriteLine("Hello Sandbox World!");
         string firstName;
         string lastName;
 
+        //console. write leaves the cursor after the after the print statement so that you can input something
         Console.Write("Enter your first name: ");
         firstName = Console.ReadLine();
 
@@ -16,5 +20,12 @@ class Program
 
         Console.WriteLine($"Your full name is {firstName} {lastName}");
 
+        int x = 10;
+
+        if (!(x == 10 || x == 12 && x == 13 && x != 23))
+        {
+            Console.WriteLine($"X is {x}");
+        }
+        
     }
 }
