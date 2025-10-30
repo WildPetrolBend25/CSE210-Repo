d7784d81c08af185058996a941740568fab86f92
BeniaminoPetrini <pet22013@byui.edu>
2025-10-30 09:16:49 -0600

    C# Prep1 code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //declare variables
+        Console.WriteLine("Hello Sandbox World!");
+        string firstName;
+        string lastName;
+
+        //enter your first name
+        Console.Write("Enter your first name: ");
+        firstName = Console.ReadLine();
+
+        //enter your last name
+        Console.Write("Enter your last name: ");
+        lastName = Console.ReadLine();
+
+        //print full name
+        Console.WriteLine($"Your full name is {firstName} {lastName}");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        string firstName;
+        string lastName;
+
+        Console.Write("Enter your first name: ");
+        firstName = Console.ReadLine();
+
+        Console.Write("Enter your last name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your full name is {firstName} {lastName}");
+
     }
 }
