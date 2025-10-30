d648a5027b92a029eee42d2843be757630a349a0
BeniaminoPetrini <pet22013@byui.edu>
2025-10-30 14:39:41 -0600

    Get User input

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        //initialize all the required variables
+        Console.Write("What is your letter grade? ");
+        string userinput = Console.ReadLine();
+        int gradePercentage = int.Parse(userinput);
+        Console.WriteLine($"This is your grade percentage {gradePercentage}");
     }
 }
