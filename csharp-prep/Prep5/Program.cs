c09089346f5a31172919ba274962bfc2098bc23a
WildPetrolBend25 <killakoiwa25@gmail.com>
2025-11-04 22:12:27 -0700

    made functioning functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,82 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // // funcrtions in C#
+        // static void DisplayMessage()
+        // {
+        //     Console.WriteLine("Hello world");
+        // }
+
+        // // accepts single string parameters
+        // static void DisplayPersonalMessage(string personalName)
+        // {
+        //     Console.WriteLine($"{personalName} personal");
+        // }
+
+        // //add numbers 
+        // static int AddNumbers(int first, int second)
+        // {
+        //     int sum = first + second;
+        //     return sum;
+        // } 
+
+        // main code
+        DisplayWelcome();
+        string username = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(favNumber);
+        int birthYear;
+        PromptUserBirthYear(out birthYear);
+
+
+        DisplayResult(username, squaredNumber, birthYear);
+
+        // print results
+        Console.WriteLine(username);
+        Console.WriteLine(favNumber);
+
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Enter your name ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your your favoritie number ");
+            string uderNumber = Console.ReadLine();
+            int favNumber = int.Parse(uderNumber);
+            return favNumber;
+        }
+
+        static void PromptUserBirthYear(out int birthYear)
+        {
+            Console.Write($"Please enter the year you were born: ");
+            birthYear = int.Parse(Console.ReadLine());
+
+        }
+
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+        static void DisplayResult(string name, int square, int birthYear)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}.");
+            Console.WriteLine($"{name}, you will turn {2025 - birthYear} years old this year.");
+        }
     }
 }
