51f6c6cae8b422f1774d6dd6544d457f8cdb86ea
WildPetrolBend25 <killakoiwa25@gmail.com>
2025-11-04 18:35:49 -0700

    make a new list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,23 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // List<string> words;
+        // make a list, declare its data type and then set aside place in memory for list
+        List<int> numbers;
+        numbers = new List<int>();
+
+        List<string> words = new List<string>();
+        words.Add("Hello");
+        words.Add("you");
+        words.Add("are");
+        words.Add("gay");
+
+        foreach (string i in words)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
