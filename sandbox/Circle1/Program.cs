921d377b48cd2fabbea62c1612646e37c3771203
BeniaminoPetrini <pet22013@byui.edu>
2025-11-11 09:54:56 -0700

    with new constructor

--- sandbox/Circle1/Program.cs
+++ sandbox/Circle1/Program.cs
@@ -3,5 +3,13 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello World");
+        Circle _myCircle = new Circle();
+
+        //_myCircle._radius = 10;
+        //how to access otherwise private data 
+        _myCircle.SetRadius(10);
+
+        Console.WriteLine(_myCircle.GetCircleArea());
+    
     }
 }
