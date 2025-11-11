df73d9cac19e17c75f4083f59ac0692db6c21b0d
BeniaminoPetrini <pet22013@byui.edu>
2025-11-11 09:54:19 -0700

    default constructor

---
+++ sandbox/Circle1/Circle.cs
@@ -0,0 +1,30 @@
+
+class Circle
+{
+    private double _radius;
+
+    //default constructor
+    public Circle()
+    {
+
+    }
+
+    public void SetRadius(double radius)
+    {
+        if (radius < 0)
+        {
+            Console.WriteLine("Invalid radius, must >= 0.0");
+            _radius = 0;
+        }
+        else
+        {
+            _radius = radius;
+            
+        }
+    }
+    
+    public double GetCircleArea()
+    {
+        return 3.1415 * _radius * _radius;
+    }
+}
