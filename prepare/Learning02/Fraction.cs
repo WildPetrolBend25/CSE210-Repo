183bf2bedfcefb1011a7c7d0df1b04ad5e312650
BeniaminoPetrini <pet22013@byui.edu>
2025-11-11 21:10:13 -0700

    get string and decimal

--- prepare/Learning02/Fraction.cs
+++ prepare/Learning02/Fraction.cs
@@ -46,4 +46,20 @@ public class Fraction
     {
         _bottomNumber = number;
     }
+
+     public string GetFractionString()
+    {
+        // Notice that this is not stored as a member variable.
+        // Is is just a temporary, local variable that will be recomputed each time this is called.
+        string text = $"{_topNumber}/{_bottomNumber}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        // Notice that this is not stored as a member variable.
+        // Is will be recomputed each time this is called.
+        return (double)_topNumber / (double)_bottomNumber;
+    }
+
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -8,11 +8,18 @@ class Program
 
         //Constructors with different arguments
         Fraction noParameters = new Fraction();
-        Fraction oneParameter = new Fraction(6);
-        Fraction twoParameters = new Fraction(6, 7);
+        Console.WriteLine($"No parameters: {noParameters.GetNumerator()} / {noParameters.GetDenominator()}");
+        Console.WriteLine(noParameters.GetFractionString());
+        Console.WriteLine(noParameters.GetDecimalValue());
 
-        Console.WriteLine($"No parameters: { noParameters.GetNumerator()} / { noParameters.GetDenominator()}");
-        Console.WriteLine($"One parameter: { oneParameter.GetNumerator()} / {oneParameter.GetDenominator()}");
+        Fraction oneParameter = new Fraction(6);
+        Console.WriteLine($"One parameter: {oneParameter.GetNumerator()} / {oneParameter.GetDenominator()}");
+        Console.WriteLine(oneParameter.GetFractionString());
+        Console.WriteLine(oneParameter.GetDecimalValue());
+
+        Fraction twoParameters = new Fraction(6, 7);
         Console.WriteLine($"Two paramters: {twoParameters.GetNumerator()} / {twoParameters.GetDenominator()}");
+        Console.WriteLine(twoParameters.GetFractionString());
+        Console.WriteLine(twoParameters.GetDecimalValue());
     }
 }
