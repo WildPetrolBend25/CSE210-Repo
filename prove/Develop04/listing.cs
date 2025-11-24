57c2e4bf01a01dd4b906855e20aa7352206f6dd0
WildPetrolBend25 <killakoiwa25@gmail.com>
2025-11-23 19:35:38 -0700

    added attributes to listing

--- prove/Develop04/listing.cs
+++ prove/Develop04/listing.cs
@@ -2,6 +2,16 @@ using System;
 
 class Listing : Activity
 {
+
+    // attributes
+    private string[] _prompts = {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+    
     public Listing(string name, string description) :base (name, description)
     {
         
