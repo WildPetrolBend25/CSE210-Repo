5e5507fa7d939fc98aabf5c369a2af0f2439d93a
BeniaminoPetrini <pet22013@byui.edu>
2025-12-03 09:53:33 -0700

    fixxed menu

--- prove/Develop05/menu.cs
+++ prove/Develop05/menu.cs
@@ -25,32 +25,40 @@ class Menu
     //methods
     public int DisplayMenu()
     {
-        //display the menue
-        foreach(string item in listMenu)
-        {
-            Console.WriteLine(item);
-        }
-
-        Console.Write("Which activity would you like to do from the menu? ");
-        string userResponse = Console.ReadLine();
-        userInput = int.Parse(userResponse);
-
+    
         while (!done)
         {
+            //display the menu
+            foreach(string item in listMenu)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Write("Which activity would you like to do from the menu? ");
+
             try
             {
-                if (userInput > 1 || userInput < 6)
+            string userResponse = Console.ReadLine();
+            userInput = int.Parse(userResponse);
+
+                if (userInput >= 1 && userInput <= 6)
                 {
                     done = true;
                 }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number");
+                    Thread.Sleep(2000); 
+                    Console.Clear();
+                }
             }
-            catch
+            catch (FormatException)
             {
-                Console.WriteLine("Please enter a valid number");
+                Console.WriteLine("Invalid input. Please enter a number between 1 and 6.");
+                Thread.Sleep(2000);
             }
         }
         return userInput;
-
     }
     
 }
