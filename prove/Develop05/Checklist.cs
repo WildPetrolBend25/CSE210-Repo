a293cc0b5ed04e2cf50aabbce2f2b4c1d86d2361
BeniaminoPetrini <pet22013@byui.edu>
2025-12-05 12:27:14 -0700

    checklist class

--- prove/Develop05/Checklist.cs
+++ prove/Develop05/Checklist.cs
@@ -4,14 +4,31 @@ using System.Runtime.CompilerServices;
 class Checklist : Goal
 {
     //attributes
-
+    private int _target;
+    private int _current;
+    private int _bonus;
     //constructors
-    public Checklist(string name, string description, int points, bool status) : base (name, description, points, status)
+    public Checklist(string name, string description, int points, bool status, int target, int bonus) : base (name, description, points, status)
     {
-        
+        _target = target;
+        _current = 0;
+        _bonus = bonus;
     }
 
     //methods
-    
+    public override int RecordEvent()
+        {
+            _current++;
+            if (_current >= _target && !IsCompleted)
+            {
+                IsCompleted = true;
+                return Points + _bonus;  // Big bonus when finished! YAY :D
+            }
+            return Points;
+        }
 
+    public override string GetStringRepresentation()
+    {
+        return base.GetStringRepresentation();
+    }
 }
